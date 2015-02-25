a011397b58898d850f985777f61efb060ea6e640
CodingOrca_cp <SND\CodingOrca_cp>
2015-02-25 13:19:48 +0000

    Initial Version

---
+++ CalendarSync/ConfigurationForm.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace LumisCalendarSync
+{
+    partial class ConfigurationForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ConfigurationForm));
+            this.comboBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.okButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.syncCommentLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBox
+            // 
+            this.comboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox.FormattingEnabled = true;
+            this.comboBox.Location = new System.Drawing.Point(15, 254);
+            this.comboBox.Name = "comboBox";
+            this.comboBox.Size = new System.Drawing.Size(412, 21);
+            this.comboBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 233);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(170, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Choose your Destination Calendar:";
+            // 
+            // okButton
+            // 
+            this.okButton.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.okButton.Location = new System.Drawing.Point(271, 318);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(75, 23);
+            this.okButton.TabIndex = 2;
+            this.okButton.Text = "OK";
+            this.okButton.UseVisualStyleBackColor = true;
+            this.okButton.Click += new System.EventHandler(this.okButton_Click);
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.cancelButton.Location = new System.Drawing.Point(352, 318);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(75, 23);
+            this.cancelButton.TabIndex = 3;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(12, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(415, 207);
+            this.label3.TabIndex = 7;
+            this.label3.Text = resources.GetString("label3.Text");
+            // 
+            // syncCommentLabel
+            // 
+            this.syncCommentLabel.Location = new System.Drawing.Point(12, 283);
+            this.syncCommentLabel.Name = "syncCommentLabel";
+            this.syncCommentLabel.Size = new System.Drawing.Size(415, 26);
+            this.syncCommentLabel.TabIndex = 8;
+            this.syncCommentLabel.Text = "The sync freuqency is controlled by outlook\'s Send/Receive All settings.\r\nYou can" +
+    " also hit F9 or press \'Send/Receive\' to trigger the sync.\r\n";
+            // 
+            // ConfigurationForm
+            // 
+            this.AcceptButton = this.okButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.cancelButton;
+            this.ClientSize = new System.Drawing.Size(439, 353);
+            this.Controls.Add(this.syncCommentLabel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.okButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox);
+            this.Name = "ConfigurationForm";
+            this.Text = "Lumis Calendar Sync Settings";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button okButton;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label syncCommentLabel;
+    }
+}
---
+++ CalendarSync/ConfigurationForm.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace LumisCalendarSync
+{
+    public partial class ConfigurationForm : Form
+    {
+        ThisAddIn outlookAddIn;
+        public ConfigurationForm(ThisAddIn addIn)
+        {
+            outlookAddIn = addIn;
+            InitializeComponent();
+            this.Load += new EventHandler(ConfigurationForm_Load);
+        }
+
+        void ConfigurationForm_Load(object sender, EventArgs e)
+        {
+            var calendars = outlookAddIn.GetPossibleDestinationCalendars();
+            string selectedItem = LumisCalendarSync.Properties.Settings.Default.DestinationCalendar;
+            if( calendars.Count > 0 )
+            {
+                foreach (string s in calendars)
+                {
+                    comboBox.Items.Add(s);
+                    if (s == selectedItem)
+                    {
+                        comboBox.SelectedItem = s;
+                    }
+                }
+            }
+        }
+
+        private void okButton_Click(object sender, EventArgs e)
+        {
+            LumisCalendarSync.Properties.Settings.Default.DestinationCalendar = comboBox.Text;
+            LumisCalendarSync.Properties.Settings.Default.Save();
+            this.Close();
+            outlookAddIn.SyncNow();
+        }
+
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}

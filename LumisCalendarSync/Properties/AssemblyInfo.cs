33e1635c53b3ab107657a348c0a3a00833d39cca
CodingOrca_cp <SND\CodingOrca_cp>
2016-10-04 13:47:43 +0000

    Change detection to reduce update frequency on remote calendar

--- LumisCalendarSync/Properties/AssemblyInfo.cs
+++ LumisCalendarSync/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("LumisCalendarSync")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Lumis Calendar Sync")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("LumisCalendarSync")]
